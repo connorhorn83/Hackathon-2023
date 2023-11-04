2a4afb91852ddffd31b1f2471416601f9030066a
lukestonefox <118642879+lukestonefox@users.noreply.github.com>
2023-11-04 15:01:56 -0400

    committed

--- App.py
+++ App.py
@@ -1,18 +1,21 @@
 from taipy import Gui
+from taipy.gui import Html
 
 img = 0
-renderResults = False
-    
+renderResults = None
+
+def handleClick(state):
+    pass
+
 page = """
 
 # Nutri*py*
 ## Upload your image here.
 
 <|{img}|file_selector|extensions=".raw,.jpeg,.png,.jpg"|>
-<|Analyze|button|on_action={lambda s: s.assign("renderResults", True)}}|>
-<|test|part|render={renderResults}|>
+<|Analyze|button|on_action=handleClick|>
+Render Results: <|{renderResults}|text|>
 
 """
-    
     
 Gui(page).run(use_reloader=True, title="Nutripy", favicon="img/nutrition.ico")
