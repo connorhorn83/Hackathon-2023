51481782f40f9365ac1c7f73171f0fff83a1b969
lukestonefox <118642879+lukestonefox@users.noreply.github.com>
2023-11-04 12:53:18 -0400

    Added favicon, metadata, file upload, analysis btn

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from taipy import Gui
 
-
+img = 0
+renderResults = False
 page = """
 
-# Upload your image *here*.
+# Nutrify
+## Upload your image *here*.
+
+<|{img}|file_selector|extensions=".raw,.jpeg,.png,.jpg"|>
+<|Analyze|button|on_action=handleAnalysis|>
 """
-Gui(page).run(use_reloader=True)
+
+def handleAnalysis(state):
+    print(f"Analyzed.")
+    renderResults = True
+    
+    
+Gui(page).run(use_reloader=True, title="Nutrify", favicon="img/nutrition.ico")
