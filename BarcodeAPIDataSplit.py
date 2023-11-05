50113aea9cfc2783c8dde8ed766734ba8b0e4db2
cbeazon0 <cbeazon@gmail.com>
2023-11-04 22:15:12 -0400

    added run_function

--- BarcodeAPIDataSplit.py
+++ BarcodeAPIDataSplit.py
@@ -69,89 +69,92 @@ def delete_text_between_square_brackets(input_string):
     result = re.sub(r'\[.*?\]', '', input_string)
     return result
 
+def run_function(image_num):
 
-files = ['Celsius_Lemon_Lime.jpg', 'Sea_Salt_Chips.jpg', 'Smokehouse_BBQ.jpg', 'image_67204609.JPG', 'Hot_Fries.jpg']
-out = files[2]
+    files = ['Celsius_Lemon_Lime.jpg', 'Sea_Salt_Chips.jpg', 'Smokehouse_BBQ.jpg', 'image_67204609.JPG', 'Hot_Fries.jpg']
+    out = files[image_num]
 
-file_path = rf'MakeUC 2023\Test Images\{out}'
+    file_path = rf'MakeUC 2023\Test Images\{out}'
 
-input_image = cv.imread(file_path)
+    input_image = cv.imread(file_path)
 
-# Used to scale the input image properly
-max_dimension = 800  # You can change this value as needed
-height, width, _ = input_image.shape
+    # Used to scale the input image properly
+    max_dimension = 800  # You can change this value as needed
+    height, width, _ = input_image.shape
 
-if width > height:
-    new_width = max_dimension
-    new_height = int(height * (max_dimension / width))
-else:
-    new_height = max_dimension
-    new_width = int(width * (max_dimension / height))
+    if width > height:
+        new_width = max_dimension
+        new_height = int(height * (max_dimension / width))
+    else:
+        new_height = max_dimension
+        new_width = int(width * (max_dimension / height))
 
-# Resize the image
-resized_image = cv.resize(input_image, (new_width, new_height))
+    # Resize the image
+    resized_image = cv.resize(input_image, (new_width, new_height))
 
-# Show the resized image
-cv.imshow('img', resized_image)
-cv.waitKey(0)
+    # Show the resized image
+    cv.imshow('img', resized_image)
+    cv.waitKey(0)
 
-# Decode the barcode
-barcodes = decode(resized_image)
+    # Decode the barcode
+    barcodes = decode(resized_image)
 
-# Iterate through the detected barcodes and print their data
-for barcode in barcodes:
-    barcode_data = barcode.data.decode('utf-8')
-    barcode_type = barcode.type
-    print(f"Barcode Type: {barcode_type}, Data: {barcode_data}")
+    # Iterate through the detected barcodes and print their data
+    for barcode in barcodes:
+        barcode_data = barcode.data.decode('utf-8')
+        barcode_type = barcode.type
+        print(f"Barcode Type: {barcode_type}, Data: {barcode_data}")
 
-if (barcode_type == 'EAN13') and len(barcodes) == 1:
-    # Send barcode data
-    barcode = barcode_data
+    if (barcode_type == 'EAN13') and len(barcodes) == 1:
+        # Send barcode data
+        barcode = barcode_data
 
-    api_key = "3uolgunto7alfggycr830aerkvyu6k"
-    url = f"https://api.barcodelookup.com/v3/products?barcode={barcode}&formatted=y&key={api_key}"
+        api_key = "3uolgunto7alfggycr830aerkvyu6k"
+        url = f"https://api.barcodelookup.com/v3/products?barcode={barcode}&formatted=y&key={api_key}"
 
-    # Sends API Call to site
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
+        # Sends API Call to site
+        with urllib.request.urlopen(url) as url:
+            data = json.loads(url.read().decode())
 
-    barcode_number = data["products"][0]["barcode_number"]
-    print("Barcode Number:", barcode_number)
+        barcode_number = data["products"][0]["barcode_number"]
+        print("Barcode Number:", barcode_number)
 
-    title = data["products"][0]["title"]
-    print("Title:", title)
-
-
-    # Extract the ingredients from the JSON data
-    ingredients = data['products'][0]['ingredients']
-
-    ingredients = remove_word(remove_word(ingredients, 'And/or'), 'And')
-    ingredients = delete_text_between_square_brackets(ingredients)
-    ingredients = split_text_with_nested_parentheses(ingredients)
-    ingredients = remove_contains_and_text_after(ingredients)
-
-    # Split ingredients into an array
-    ingredient_list = ingredients.split(', ')
-
-    # Add spaces before and after each ingredient, replacing '\n' with a space
-    ingredient_list_with_spaces = [' ' + ingredient.strip().replace('\n', ' ') + ' ' for ingredient in ingredient_list]
-
-    # Split the list elements based on double spaces
-    final_ingredients = []
-    for ingredient in ingredient_list_with_spaces:
-        parts = ingredient.split('  ')  # Split on double spaces
-        final_ingredients.extend(parts)
-
-    # Filter out empty strings
-    final_ingredients = [ingredient for ingredient in final_ingredients if ingredient.strip()]
-
-    # Print the array of ingredients with double space separation
-    print("Ingredients:")
+        title = data["products"][0]["title"]
+        print("Title:", title)
 
 
-    for i in range(len(final_ingredients)):
-        final_ingredients[i] = remove_characters(final_ingredients[i], '/*()[].,:?')
+        # Extract the ingredients from the JSON data
+        ingredients = data['products'][0]['ingredients']
+
+        ingredients = remove_word(remove_word(ingredients, 'And/or'), 'And')
+        ingredients = delete_text_between_square_brackets(ingredients)
+        ingredients = split_text_with_nested_parentheses(ingredients)
+        ingredients = remove_contains_and_text_after(ingredients)
+
+        # Split ingredients into an array
+        ingredient_list = ingredients.split(', ')
+
+        # Add spaces before and after each ingredient, replacing '\n' with a space
+        ingredient_list_with_spaces = [' ' + ingredient.strip().replace('\n', ' ') + ' ' for ingredient in ingredient_list]
+
+        # Split the list elements based on double spaces
+        final_ingredients = []
+        for ingredient in ingredient_list_with_spaces:
+            parts = ingredient.split('  ')  # Split on double spaces
+            final_ingredients.extend(parts)
+
+        # Filter out empty strings
+        final_ingredients = [ingredient for ingredient in final_ingredients if ingredient.strip()]
+
+        # Print the array of ingredients with double space separation
+        print("Ingredients:")
+
+
+        for i in range(len(final_ingredients)):
+            final_ingredients[i] = remove_characters(final_ingredients[i], '/*()[].,:?')
 
 
 
-    # Final Ingredients has all of the sorted data
+        # Final Ingredients has all of the sorted data
+        return final_ingredients
+    
