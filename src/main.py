0420edd5939174ddef9f07c9516e0e7c0d339002
lukestonefox <118642879+lukestonefox@users.noreply.github.com>
2023-11-04 20:42:33 -0400

    did shit

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from taipy import Gui
-from webcam import Webcam
 from data.ComponentGeneralUse_Dataparse import search_components
 
 test_list = ['Biotin', ' ascorbic acid', 'pantothenic']
 
 df = search_components(test_list)
-
+queryInput = ""
 url = None
 # TODO: figure out the fuckin picture
 page = """
@@ -15,13 +14,17 @@ Upload an image of your 'Nutrition Facts' to get nutritional information about t
 
 <|{url}|file_selector|label=Upload Image|extensions=".raw,.jpeg,.png,.jpg,.raw"|>
 <|Analyze|button|on_action=analyze|>
-
-<|{url}|image|>
+<|{queryInput}|input|label=Search...|>
 
 <|{df}|table|>
 
 """
-
+def on_change(state, var_name, var_value):
+    if var_name == "queryInput":
+        state.queryInput = ''
+        print(queryInput)
+        return
+        
 def analyze(state):
     # anaylyze shit
     pass
