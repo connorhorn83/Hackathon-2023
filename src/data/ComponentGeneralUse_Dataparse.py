077ff920cee4803919a6341b6faf387068a1b095
connorhorn83 <horncw@mail.uc.edu>
2023-11-04 22:29:32 -0400

    commit

--- src/data/ComponentGeneralUse_Dataparse.py
+++ src/data/ComponentGeneralUse_Dataparse.py
@@ -7,20 +7,25 @@ Created on Sat Nov  4 13:48:05 2023
 
 import pandas as pd
 import re
-import numpy as np
+from BarcodeAPIDataSplit import run_function
+
 
 def clean_text(text):
-    # Remove special characters and keep only letters, numbers, spaces, and basic punctuation
-    cleaned_text = re.sub(r'[^\w\s.,!?]', '', text)
-    cleaned_text = cleaned_text.replace('<br />', '')  # Remove <br />
-    cleaned_text = cleaned_text.replace(',br ', ', ')  # Remove <br /
-    return cleaned_text
+    # Ensure 'text' is a string before cleaning
+    if isinstance(text, str):
+        # Remove special characters and keep only letters, numbers, spaces, and basic punctuation
+        cleaned_text = re.sub(r'[^\w\s.,!?]', '', text)
+        cleaned_text = cleaned_text.replace('<br />', '')  # Remove <br />
+        cleaned_text = cleaned_text.replace(',br ', ', ')  # Remove <br /
+        return cleaned_text
+    else:
+        return str(text)  # Convert non-string data to string and return
 
 def search_components(components_list):
-    file_path = "src\data\FoodSubstances.xlsx"  # Replace with the path to your Excel file
-    sheet_name = 'FoodSubstances'  # Replac with the name of your sheet if different
+    file_path = r"C:/Users/conno/OneDrive/Desktop/Hackathon/Hackathon-2023/src/data/FoodSubstances.xlsx"
+    sheet_name = 'FoodSubstances'
     columns_of_interest = ['Name', 'Use']
-    df = pd.DataFrame()
+    df_list = []  # Empty list to store individual DataFrames
 
     try:
         data = pd.read_excel(file_path, sheet_name=sheet_name)
@@ -31,28 +36,35 @@ def search_components(components_list):
 
         for component in components_list:
             print(f"Searching for '{component}':")
-            search_item = component.lower()
-            found_items = indexed_data[indexed_data.index.str.contains(search_item, case=False, na=False)]
-            df = df._append(found_items, ignore_index=True)
+            search_item = r"\b" + re.escape(component.lower()) + r"\b"  # Use regex word boundaries
+            found_items = indexed_data[indexed_data.index.str.contains(search_item, case=False, na=False, regex=True)]
 
             if not found_items.empty:
+                df_list.append(found_items)  # Append found DataFrame slices to the list
                 print("Results:")
                 for index, row in found_items.iterrows():
-                    cleaned_name = clean_text(index)  # Clean the 'Name' column text
-                    cleaned_use = clean_text(row['Use'])  # Clean the 'Use' column text
+                    cleaned_name = clean_text(str(index))  # Clean the 'Name' column text
+                    cleaned_use = clean_text(str(row['Use']))  # Clean the 'Use' column text
                     print(f"Name: {cleaned_name}, Use: {cleaned_use}")
             else:
                 print("No items found matching the search criteria for", component)
             print("\n")
 
-        df['Use'] = df['Use'].apply(clean_text)
-        return df
+        df_combined = pd.concat(df_list, ignore_index=True) if df_list else pd.DataFrame()  # Combine individual DataFrames into a single DataFrame
+
+        df_combined['Use'] = df_combined['Use'].apply(clean_text)
+        return df_combined
 
     except FileNotFoundError:
         print("File not found. Please provide the correct file path.")
     except Exception as e:
         print("An error occurred:", e)
 
-# Example usage: Call the function and pass a list of components from another method (temp)
-temp = ['Biotin', ' ascorbic acid', 'pantothenic']
-search_components(temp)
+    # Example usage: Call the function and pass a list of components from another method (temp)
+def ingredient_list_search():
+    final_ingredients = run_function(1)
+    temp = final_ingredients
+    search_components(temp)
+
+ingredient_list_search()
+    
